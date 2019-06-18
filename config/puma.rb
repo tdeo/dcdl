f7c2ea3bb18c95da1153ebdace4e70ace92951b2
Thierry Deo <thierry.deo@gmail.com>
2019-06-18 15:19:40 +0200

    Change default port

--- config/puma.rb
+++ config/puma.rb
@@ -3,5 +3,6 @@ threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 4567
 environment ENV['RACK_ENV'] || 'development'
+quiet false

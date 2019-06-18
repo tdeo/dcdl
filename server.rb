bdbe447c782448ed4ef7d975e014999c0e25af55
Thierry Deo <thierry.deo@gmail.com>
2019-06-18 15:05:13 +0200

    CORS

--- server.rb
+++ server.rb
@@ -1,11 +1,33 @@
-require 'sinatra'
 require 'json'
+
+require 'sinatra'
+require 'sinatra/cross_origin'
 
 require './dcdl'
 
-post '/solve' do
-  request.body.rewind
-  data = JSON.parse(request.body.read)
-  solver = Dcdl.new(data['target'])
-  return JSON.dump(solver.solve(data['numbers']))
+class MyApp < Sinatra::Base
+  set :bind, '0.0.0.0'
+
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  post '/solve' do
+    request.body.rewind
+    data = JSON.parse(request.body.read)
+    solver = Dcdl.new(data['target'])
+    return JSON.dump(solver.solve(data['numbers']))
+  end
+
+
+  options '*' do
+    response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    200
+  end
 end
